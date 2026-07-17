fd0460612affaff54fbc298a5c8850bcf2982078
SunsetStand <1261584939@qq.com>
2026-07-17 12:44:47 -0400

    fix: overlap matrix dimension when P grows

--- scripts_new/phaseA_cas10_v10_sacis.py
+++ scripts_new/phaseA_cas10_v10_sacis.py
@@ -421,7 +421,7 @@ while N_p < P_MAX:
         tracked_roots = list(range(ns))
     else:
         n_ref = min(C_ref_track.shape[1], ns)
-        overlap = np.abs(C_ref_track[:, :n_ref].T @ C_P[:, :ns])
+        overlap = np.abs(C_ref_track[:, :n_ref].T @ C_P[:Np_old, :ns])
         assigned = set()
         tracked_roots = []
         for j in range(ns):
